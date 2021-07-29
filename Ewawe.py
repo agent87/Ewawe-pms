bbb154136a2624b4e5d4ca29251809c4f08e1fda
agent87 <arnauldkayonga1@gmail.com>
2021-07-29 16:39:05 +0200

    Update

--- Ewawe.py
+++ Ewawe.py
@@ -73,10 +73,10 @@ def parking_log(Plate):
     if query:
         elapsed = (time.time()- query[0][5]) /60
         #update Checkoutimte, status, cost  and duration
-        cur.execute(f""" UPDATE public."Auth_parkinglog" SET "CheckoutTime"='{time.time()}', "ExitGateId"='Main', "Status"='Exited', "Duration"={int(str(elapsed.seconds))}, "Cash"={int(str(cost(int(elapsed.seconds))))} WHERE "TicketId"=uuid'{query[0][0]}'; """)
+        cur.execute(f""" UPDATE public."Auth_parkinglog" SET "CheckoutTime"= {time.time()}, "ExitGateId"='Main', "Status"='Exited', "Duration"={elapsed} WHERE "TicketId"=uuid'{query[0][0]}'; """)
         conn.commit()
     else:
-        cur.execute(f"""INSERT INTO public."Auth_parkinglog" ("TicketId", "CustomerId", "Date", "PlateNum", "EntryGateId", "CheckinTime", "CheckoutTime", "ExitGateId", "Status", "Duration", "Cash") VALUES(uuid'{uuid.uuid4()}', 'EGPCI-AAA01-0001', date'{str(datetime.datetime.now().date())}', '{Plate}','Main', '{time.time()}', Null, Null, 'Parked', Null, Null);""")
+        cur.execute(f"""INSERT INTO public."Auth_parkinglog" ("TicketId", "CustomerId", "Date", "PlateNum", "EntryGateId", "CheckinTime", "CheckoutTime", "ExitGateId", "Status", "Duration", "Cash") VALUES(uuid'{uuid.uuid4()}', 'EGPCI-AAA01-0001', date'{str(datetime.datetime.now().date())}', '{Plate}','Main', {time.time()}, Null, Null, 'Parked', Null, Null);""")
         conn.commit()
     #If it is then update the time stamp, Exit Gate, Status & duration
     #else add the Plate to the database
