fd7bbe5795461778cc0411366f7a9e20d492a454
agent87 <arnauldkayonga1@gmail.com>
2021-07-29 16:48:46 +0200

    Update

--- Ewawe.py
+++ Ewawe.py
@@ -72,8 +72,17 @@ def parking_log(Plate):
     query = cur.fetchall()
     if query:
         elapsed = (time.time()- query[0][5]) /60
+        if elapsed <= 30:
+            cash = 300
+        elif elapsed <= 60:
+            cash  = 500
+        elif elapsed <= 90:
+            cash = 700
+        else:
+            cash = 1000
+        
         #update Checkoutimte, status, cost  and duration
-        cur.execute(f""" UPDATE public."Auth_parkinglog" SET "CheckoutTime"= {time.time()}, "ExitGateId"='Main', "Status"='Exited', "Duration"={elapsed} WHERE "TicketId"=uuid'{query[0][0]}'; """)
+        cur.execute(f""" UPDATE public."Auth_parkinglog" SET "CheckoutTime"= {time.time()}, "ExitGateId"='Main', "Status"='Exited', "Duration"={elapsed}, Cash={cash} WHERE "TicketId"=uuid'{query[0][0]}'; """)
         conn.commit()
     else:
         cur.execute(f"""INSERT INTO public."Auth_parkinglog" ("TicketId", "CustomerId", "Date", "PlateNum", "EntryGateId", "CheckinTime", "CheckoutTime", "ExitGateId", "Status", "Duration", "Cash") VALUES(uuid'{uuid.uuid4()}', 'EGPCI-AAA01-0001', date'{str(datetime.datetime.now().date())}', '{Plate}','Main', {time.time()}, Null, Null, 'Parked', Null, Null);""")
